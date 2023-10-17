098ba6829b21ce4f288f7f1b58fe990c93c3fc16
Kristóf Kálai <kristof.kalai@supercharge.io>
2023-10-17 15:53:57 +0200

    feat: add bounciness control

--- Sources/CompactScrollView/CompactScrollView.swift
+++ Sources/CompactScrollView/CompactScrollView.swift
@@ -9,14 +9,44 @@ import SizePreferenceKey
 import SwiftUI
 
 public struct CompactScrollView<Content: View> {
+    public enum Bounciness {
+        case alwaysBounces
+        case neverBounces
+        case adaptive
+    }
+
     private let axes: Axis.Set
     private let showsIndicators: Bool
+    private let bounciness: Bounciness
     private let content: () -> Content
-    @State private var contentSize: CGSize = .zero
+    @State private var contentSize: CGSize?
+    @State private var scrollSize: CGSize?
 
-    public init(_ axes: Axis.Set = .vertical, showsIndicators: Bool = true, @ViewBuilder content: @escaping () -> Content) {
+    private var width: CGFloat? {
+        if let contentSize, let scrollSize {
+            min(scrollSize.width, contentSize.width)
+        } else {
+            nil
+        }
+    }
+
+    private var height: CGFloat? {
+        if let contentSize, let scrollSize {
+            min(scrollSize.height, contentSize.height)
+        } else {
+            nil
+        }
+    }
+
+    public init(
+        _ axes: Axis.Set = .vertical,
+        showsIndicators: Bool = true,
+        bounciness: Bounciness = .adaptive,
+        @ViewBuilder content: @escaping () -> Content
+    ) {
         self.axes = axes
         self.showsIndicators = showsIndicators
+        self.bounciness = bounciness
         self.content = content
     }
 }
@@ -25,11 +55,27 @@ extension CompactScrollView: View {
     public var body: some View {
         ScrollView(axes, showsIndicators: showsIndicators) {
             content()
-                .storeSize(in: $contentSize)
+                .storeSize(in: $contentSize, block: setBounciness)
         }
-        .frame(maxWidth: contentSize.width, maxHeight: contentSize.height)
-        .readSize {
-            UIScrollView.appearance().bounces = $0.height < contentSize.height
+        .frame(width: axes.contains(.vertical) ? nil : width, height: axes.contains(.horizontal) ? nil : height)
+        .storeSize(in: $scrollSize, block: setBounciness)
+    }
+}
+
+extension CompactScrollView {
+    private func setBounciness() {
+        let bounces: Bool? = switch bounciness {
+        case .alwaysBounces: true
+        case .neverBounces: false
+        case .adaptive:
+            if let contentSize, let scrollSize {
+                scrollSize.height < contentSize.height
+            } else {
+                nil
+            }
+        }
+        if let bounces {
+            UIScrollView.appearance().bounces = bounces
         }
     }
 }
